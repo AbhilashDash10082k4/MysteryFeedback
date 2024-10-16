c8d77b2596a80cfbd2f3130c4036bf4ec68e1542
AbhilashDash10082k4 <abhilash10082k4@gmail.com>
2024-10-16 16:21:13 +0530

    done with routing logic

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+'use client'
+import { useSession, signIn, signOut } from "next-auth/react"
 
-function page() {
+export default function Component() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
   return (
-    <div>
-      Page
-    </div>
+    <>
+      Not signed in <br />
+      <button className="bg-blue-600 p-3 px-4 rounded-xl" onClick={() => signIn()}>Sign in</button>
+    </>
   )
 }
-
-export default page
