b5b65368c4bc2c65ed98e6d1c68583635a3f180a
AbhilashDash10082k4 <abhilash10082k4@gmail.com>
2024-10-23 17:33:55 +0530

    user can accept messages and all messages of user can be shown

---
+++ src/app/api/accept-message/route.ts
@@ -0,0 +1,90 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+//to show the msgs of logged in user
+//to know which user is logged in, sessions are to be used
+//user should be also able to send msgs
+
+import { getServerSession } from "next-auth"; // take sesssion from backend and whatever data is reqd it is taken by query call, retrieves the current session on the server side.
+import { authOptions } from "../auth/[...nextauth].ts/option";// FOR CREDENTIALS, to configure how the session is fetched.
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { User } from "next-auth"; //to get authenticated user
+
+//post request to allow user to toggle the button whether he wants to accept random msgs or not
+export async function POST(request: Request) {
+    await dbConnect();
+    //getting logged in user, take user is and take all its info by query
+    //1. getting session
+    const session = await getServerSession(authOptions);
+    const user: User = session?.user as User //injected user in session in authOptions, as assertion is used
+    if(!session || !session.user) {
+        return Response.json({
+            success: false,
+            message: "User is not authenticated"
+        }, {status: 401});
+    }
+    const userId = user._id
+    //request to backend from user whether to recieve messages
+    const {acceptMessages} = await request.json();  
+
+    try {
+        const updatedUser = await UserModel.findByIdAndUpdate(user, {isAcceptingMessages: acceptMessages}, {new: true}) //find user by id and update it on the basis of accepting messages
+        if(!updatedUser) {
+            console.log("Failed to update user status to accept message")
+            return Response.json({
+                success: false,
+                message: "Failed to update user status to accept message"
+            }, {status: 401});
+        }
+        console.log("Successfuly updated user status to accept message")
+        return Response.json({
+            success: true,
+            message: "Successfuly updated user status to accept message",
+            updatedUser,
+        }, {status: 200});
+
+    } catch (error) {
+        console.log("Failed to update user status to accept message")
+        return Response.json({
+            success: false,
+            message: "Failed to update user status to accept message"
+        }, {status: 500});
+    }
+}
+
+//GET method to query from db and send status
+export async function GET(request: Request) {
+    await dbConnect();
+    //getting logged in user, take user is and take all its info by query
+    //1. getting session
+    const session = await getServerSession(authOptions);
+    const user: User = session?.user as User 
+    if(!session || !session.user) {
+        console.log("Failed to...")
+        return Response.json({
+            success: false,
+            message: "User is not authenticated"
+        }, {status: 401});
+    }
+    const userId = user._id
+    try {
+        const foundUserById = await UserModel.findById(user);
+        if(!foundUserById) {
+            console.log("Failed to...")
+            return Response.json({
+                success: false,
+                message: "User not found"
+            }, {status: 401});
+        }
+    
+        return Response.json({
+            success: true,
+            isAcceptingMessages: foundUserById.isAcceptingMessage //if the user found by id is accepting messages
+        }, {status: 200});
+    } catch (error) {
+        console.log("Failed to...")
+        return Response.json({
+            success: false,
+            message: "User not found"
+        }, {status: 401});
+    }
+}
---
+++ src/app/api/get-message/route.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth].ts/option";// FOR CREDENTIALS, to configure how the session is fetched.
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { User } from "next-auth"; //to get authenticated user
+import mongoose from "mongoose";
+
+export async function GET( request: Request ) {
+    await dbConnect();
+    //getting session
+    //if the user is logged in
+    const session = await getServerSession(authOptions);
+    const user: User = session?.user as User;
+    if(!session || !session.user) {
+        return Response.json({
+            success: false,
+            message: "User is not authenticated"
+        }, {status: 401});
+    }
+    const userId = new mongoose.Types.ObjectId(user._id) //Mongoose uses ObjectId for performing queries, so converting a string _id to ObjectId ensures that you can use it in MongoDB operations such as queries, updates, or deletions.
+    try {
+        //getting messages, using aggregation pipeline
+        const user = await UserModel.aggregate([
+            {$match: {id: userId}}, //matching user with the derived userId
+            {$unwind: '$messages'}, //unwinding array to get object of user which has messages,id and createdAt 
+            {$sort: {'messages.createsAt': -1}}, //sorting messages in descending order
+            {$group: {_id: '_id', messages:{$push: '$messages'}}} //grp by id and push the sorted messages'
+        ])
+        if(!user || user.length === 0) {
+            return Response.json({
+                success: false,
+                message: "User is not authenticated"
+            }, {status: 401});
+        }
+        return Response.json({
+            success: true,
+            message: user[0].messages //return type of aggregate is an user
+        }, {status: 200});
+    } catch (error) {
+        
+    }
+}
